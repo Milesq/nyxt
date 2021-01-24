0f05b2a5459d9c0e4e32f93bb4a7c52434d9c07b
Milesq <wmilosz88@gmail.com>
2021-01-25 00:19:16 +0100

    fix: fix 404

--- src/Nyxt.php
+++ src/Nyxt.php
@@ -4,18 +4,22 @@ namespace Nyxt;
 final class Base {
     public $router;
 
-    function __construct() {
+    function __construct($get_pdo_fn = NULL) {
         $this->router = new \Bramus\Router\Router();
 
         $this->router->set404(function() {
-            $file404 = './template/[404].html';
+            $file404 = './public/404.html';
+            $error_template = './templates/[error].html';
 
             if (file_exists($file404)) {
+                echo file_get_contents($file404);
+            } elseif (file_exists($error_template)) {
                 $loader = new \Twig\Loader\ArrayLoader([
-                    '404' => file_get_contents($file404),
+                    'error' => file_get_contents($error_template),
                 ]);
                 $twig = new \Twig\Environment($loader);
-                $twig->render('404', [ 'why' => 'not found' ]);
+
+                echo $twig->render('error', ['why' => 'not found', 'errcode' => '404']);
             } else {
                 echo '404 - Not Found';
             }
@@ -30,12 +34,12 @@ final class Base {
         });
 
         foreach (scan_controllers('controllers') as $entry) {
-            $this->router->all($entry[1], function(...$args) use ($entry) {
+            $this->router->all($entry[1], function(...$args) use ($entry, $get_pdo_fn) {
                 include $entry[0];
                 $matches = [];
                 preg_match_all('/\{([^\}]+)\}/', $entry[1], $matches);
 
-                $h = new \Handler(array_combine($matches[1], $args));
+                $h = new \Handler(array_combine($matches[1], $args), $get_pdo_fn);
                 $h->handle();
             });
         }
