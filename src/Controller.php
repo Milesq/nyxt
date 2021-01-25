d2f20fbed2f05082860ad7bc1d57918e3e4bf448
Milesq <wmilosz88@gmail.com>
2021-01-25 16:35:24 +0100

    refactor: remove commented code

--- src/Controller.php
+++ src/Controller.php
@@ -4,7 +4,6 @@ namespace Nyxt;
 use \Rakit\Validation\Validator;
 
 abstract class Controller extends Internal\AssocArrayObjectSyntax {
-    // private $orm_obj = NULL;
     abstract public function handle();
 
     /**
@@ -33,28 +32,6 @@ abstract class Controller extends Internal\AssocArrayObjectSyntax {
             die();
         }
     }
-
-    // protected function getOrm() {
-    //     if ($this->orm_obj === NULL) {
-    //         $pdo = $this->get_pdo_fn;
-    //         if ($pdo === NULL) {
-    //             throw new \Exception('PDO connection getter was not declared');
-    //         }
-
-    //         $this->orm_obj = new \ClanCats\Hydrahon\Builder('mysql', function($query, $queryString, $queryParameters) use($pdo) {
-    //             echo $queryString;
-    //             // $statement = $pdo->prepare($queryString);
-    //             // $statement->execute($queryParameters);
-
-    //             if ($query instanceof \ClanCats\Hydrahon\Query\Sql\FetchableInterface) {
-    //                 // return $statement->fetchAll();
-    //                 return ['e' => 2];
-    //             }
-    //         });
-    //     }
-
-    //     return $this->orm_obj;
-    // }
 
     protected function try_render(string $name, array $args = []) {
         try {
